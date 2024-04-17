6e4683d22ba8e1f2fc0bd955e2f70dd2a75d7e18
gamabuntat <schoshoev@yandex.ru>
2024-04-18 01:53:27 +0800

    add Button?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from '@/components/ui/button';
+import Heart from '@/assets/heart.svg';
 
 export default function Home() {
   return (
-    <>
-      <Button className="btn bg-amber-400 hover:bg-amber-400 text-black border-black border-solid border-2 relative">
-        Click me!
-      </Button>
-    </>
+    <Button className="gap-1">
+      <Heart className="w-4 stroke-current fill-danger" />
+      <span>Love button</span>
+    </Button>
   );
 }
